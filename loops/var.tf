64efd10820c77703cf2222e4545b42b5605ef8a3
chanakya <you@example.com>
2026-08-24 14:29:43 +0530

    terraform

--- loops/var.tf
+++ loops/var.tf
@@ -2,3 +2,15 @@ variable "instances" {
     type = list
     default = ["mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend"]
 }
+
+
+variable "zone_id" {
+    type = string
+    default = "Z027359523QFZK0V41S4X"
+  
+}
+
+variable "domain_id" {
+    default = "cineniti.in"
+  
+}
